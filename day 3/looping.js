f13ccf008dda4aa781ef05264d1bf4afe8d43002
Harshithamarthala <work.harshithamarthala@gmai.com>
2025-01-08 13:19:04 +0530

    new

---
+++ day 3/looping.js
@@ -0,0 +1,15 @@
+for (let i=0;i<=5;i++){
+    if(i%2 ==0){
+        console.log(i +" even");  
+    }
+    else{
+        console.log(i +" odd");
+    }
+}
+
+let a=Number(prompt("Enter a number"));
+let r=1;
+for( let i=1;i<=a;i++)
+    r*=i;
+console.log(r);
+
