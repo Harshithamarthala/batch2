62d3b20a48f219cdbc756441dc4695fc704c4315
Harshithamarthala <work.harshithamarthala@gmai.com>
2025-01-09 10:22:16 +0530

    new

---
+++ day 3/function.js
@@ -0,0 +1,14 @@
+function test1(a,b){
+    console.log("hii");
+    
+    return a+b;
+}
+console.log(test1(5,5));
+
+let x=(a,b) => {return a*b};
+console.log(x(5,5));
+
+let y= (a,b) => a+b;
+console.log(y(100,200));
+
+
