ee0891a3bbfd4866f7fa1e0acd2a3224b07d57cd
Harshithamarthala <work.harshithamarthala@gmail.com>
2025-01-10 18:24:50 +0530

    programs

--- day 3/function.js
+++ day 3/function.js
@@ -1,14 +1,28 @@
-function test1(a,b){
-    console.log("hii");
+// function test1(a,b){
+//     console.log("hii");
+//     return a+b;
+// }
+// console.log(test1(5,5));
+
+// let x=(a,b) => {return a*b};
+// console.log(x(5,5));
+
+// let y= (a,b) => a+b;
+// console.log(y(100,200));
+
+function hof(a) {
+    return a();
     
-    return a+b;
 }
-console.log(test1(5,5));
+let x=hof(function(){return "callback function"});
+console.log(x);
 
-let x=(a,b) => {return a*b};
-console.log(x(5,5));
+let y=function(){return "callback function"};
+console.log(y());
 
-let y= (a,b) => a+b;
-console.log(y(100,200));
+let users=["Harshi","Yashu","bindu","deekshu"];
+console.log(users);
+users.map(function(user){console.log(user);
+})
 
 
