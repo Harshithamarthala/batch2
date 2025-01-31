4b26d3f1efc0f6eed723fe07f0cc118042d5c3db
Harshithamarthala <work.harshithamarthala@gmail.com>
2025-01-31 11:42:04 +0530

    modules

---
+++ app.js
@@ -0,0 +1,15 @@
+import x from "./child.js"
+console.log(x);
+// import collage,{x,user,users,obj} from "./child";
+
+// console.log(collage);
+// console.log(x);
+// console.log(user);
+
+
+// console.log(users);
+// users.map(user=>{
+//     console.log(user)
+// })
+
+// console.log(obj.company);
---
+++ child.js
@@ -0,0 +1,41 @@
+let x=10;
+export default x;
+
+// let userDetail={
+//     name:"Sathvik",
+//     profession:"CR"
+// }
+// export default userDetail;
+
+
+// let x=10;
+// const user="Tanuja";
+// const users=["satvik","Lokesh","Harshi","kowshik","mahesh"];
+
+// const obj={
+//     name:"sam",
+//     company:"Excelr"
+// }
+
+// export{
+//     x,
+//     user,
+//     users,
+//     obj
+
+// }
+
+
+// export let x=10;
+
+// export const user="Tanuja";
+
+// export const users=["satvik","Lokesh","Harshi","kowshik","mahesh"];
+
+// export const obj={
+//     name:"sam",
+//     company:"excelR"
+// }
+
+// let collage="MallaReddy University";
+// export default collage;
